25531ab4e8bb2b98e9ad5ad34fdafb0a2432aa88
Sagramielgo <sagramielgo@gmail.com>
2021-04-22 16:31:02 +0200

    Create sounds folder and playsound function

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
-window.addEventListener('keydown', function (e) {
-  console.log(e);
-});
+'use strict';
+
+const keys = document.querySelectorAll('.key');
+
+function playSound(ev) {
+  const audio = document.querySelector(`audio[data-key="${ev.keycode}"]`);
+
+  const key = document.querySelector(`key[data-key="${ev.keycode}"]`);
+}
+
+window.addEventListener('keydown', playSound);
