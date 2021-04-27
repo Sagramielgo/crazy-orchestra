52ebcf52492c81fb8cd046f9b470ce37905fb253
Sagramielgo <sagramielgo@gmail.com>
2021-04-27 18:30:54 +0200

    fix function and add styles for playing class

--- js/main.js
+++ js/main.js
@@ -1,11 +1,12 @@
 'use strict';
 
-window.addEventListener('keydown', function (e) {
-  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+window.addEventListener('keydown', function (ev) {
+  const audio = document.querySelector(`audio[data-key="${ev.keyCode}"]`);
+  const key = document.querySelector(`.key[data-key="${ev.keyCode}"]`);
   if (!'audio') return;
   audio.currentTime = 0;
   audio.play();
-  key.classList.Add('playing');
+  key.classList.add('playing');
 });
 
 /* const keys = document.querySelectorAll('.key');
